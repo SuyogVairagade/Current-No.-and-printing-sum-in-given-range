3c1e03347fc0048f1c4a2d999a93152ede63a2dd
SuyogVairagade <63891865+SuyogVairagade@users.noreply.github.com>
2020-06-05 10:58:14 +0530

    Add files via upload
    Source Code
    print("Printing current and previous number sum in a given range(10)")
    r=1
    c=1
    n=0
    while  n in range(6):
        print("Current Number ",n,"Previous Number",pv,"Sum: ",s)
        n=n+1
        pv=n-1
        s=n+pv

---
+++ cur_no_prev_no_sum.py
@@ -0,0 +1,9 @@
+print("Printing current and previous number sum in a given range(10)")
+r=1
+c=1
+n=0
+while  n in range(6):
+    print("Current Number ",n,"Previous Number",pv,"Sum: ",s)
+    n=n+1
+    pv=n-1
+    s=n+pv
